f5f1d40a335af33b68258ea00d51b63d105ae031
ColdClear <3124364150@qq.com>
2023-08-30 18:39:46 +0800

    merged  origin

---
+++ arklet/arklet-springboot-actuator/src/test/java/com/alipay/sofa/serverless/arklet/springboot/actuator/ArkletSpringbootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.serverless.arklet.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkletSpringbootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
