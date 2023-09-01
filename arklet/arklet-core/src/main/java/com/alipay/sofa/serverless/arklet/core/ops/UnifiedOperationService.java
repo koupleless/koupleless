d6026295de9a1ac2f3c95987065220bc4dd06e47
mingmen <mingmen.zjm@alibaba-inc.com>
2023-09-01 17:31:19 +0800

    append doc

--- arklet/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/ops/UnifiedOperationService.java
+++ arklet/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/ops/UnifiedOperationService.java
@@ -23,17 +23,43 @@ import com.alipay.sofa.ark.spi.model.Biz;
 import com.alipay.sofa.serverless.arklet.core.ArkletComponent;
 
 /**
+ * 统一操作服务接口
  * @author mingmen
  * @date 2023/6/14
  */
 public interface UnifiedOperationService extends ArkletComponent {
 
+
+    /**
+     * install biz
+     * @param bizUrl biz URL
+     * @return response
+     * @throws Throwable error
+     */
     ClientResponse install(String bizUrl) throws Throwable;
 
+    /**
+     * uninstall biz
+     * @param bizName bizName
+     * @param bizVersion bizVersion
+     * @return response
+     * @throws Throwable error
+     */
     ClientResponse uninstall(String bizName, String bizVersion) throws Throwable;
 
+    /**
+     * query biz list
+     * @return biz list
+     */
     List<Biz> queryBizList();
 
+    /**
+     * switch biz
+     * @param bizName bizName
+     * @param bizVersion bizVersion
+     * @return response
+     * @throws Throwable error
+     */
     ClientResponse switchBiz(String bizName, String bizVersion) throws Throwable;
 
 }
