b87d7e6302ac252e149c71f9e71867cdfd8a9d2f
charlie <qianglin98@qq.com>
2023-09-04 21:31:32 +0800

    test: add unit test
    Signed-off-by: charlie <qianglin98@qq.com>

--- module-controller/internal/utils/controller_utils_test.go
+++ module-controller/internal/utils/controller_utils_test.go
@@ -1,13 +1,19 @@
 package utils
 
 import (
-	"github.com/sofastack/sofa-serverless/internal/constants/finalizer"
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"testing"
-	"time"
+
+	"github.com/sofastack/sofa-serverless/internal/constants/finalizer"
+	"github.com/sofastack/sofa-serverless/internal/constants/label"
 )
 
 func TestAddNotExistedFinalizer(t *testing.T) {
@@ -65,4 +71,22 @@ func TestGetNextReconcileTime(t *testing.T) {
 	h1, _ := time.ParseDuration("-61m")
 	reconcileTime4 := GetNextReconcileTime(now.Add(h1))
 	assert.Equal(t, "10m0s", reconcileTime4.String())
+}
+
+func TestGetModuleCountFromPod(t *testing.T) {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{},
+		},
+	}
+	count := 5
+
+	for i := 0; i < count; i++ {
+		pod.Labels[fmt.Sprintf("%s-%s", label.ModuleNameLabel, "module-"+strconv.Itoa(i))] = "1.0.0"
+	}
+
+	actual := GetModuleCountFromPod(pod)
+	if count != actual {
+		t.Errorf("the expected count is %v, but got %v", count, actual)
+	}
 }
