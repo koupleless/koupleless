5216b72cc8712a56ef221fe0d917b1b2ef142760
ColdClear <3124364150@qq.com>
2023-09-10 05:17:42 +0800

    optimized arklet healthService update README.md

--- arklet/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/health/indicator/CpuIndicator.java
+++ arklet/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/health/indicator/CpuIndicator.java
@@ -3,6 +3,7 @@ package com.alipay.sofa.serverless.arklet.core.health.indicator;
 import com.alipay.sofa.serverless.arklet.core.health.model.Constants;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
+import oshi.util.GlobalConfig;
 import oshi.util.Util;
 
 import java.util.HashMap;
@@ -49,10 +50,10 @@ public class CpuIndicator extends ArkletBaseIndicator {
 
         public CpuIndicatorHandler() {
             this.cpu = new SystemInfo().getHardware().getProcessor();
+            prevTicks = cpu.getSystemCpuLoadTicks();
         }
 
         public void collectTicks() {
-            prevTicks = cpu.getSystemCpuLoadTicks();
             nextTicks = cpu.getSystemCpuLoadTicks();
         }
 
