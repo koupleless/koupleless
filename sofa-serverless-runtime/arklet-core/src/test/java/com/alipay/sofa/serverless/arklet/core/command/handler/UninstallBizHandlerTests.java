d146fb26d6e36e4ed5b2b4468da856b1f058fb2c
ColdClear <3124364150@qq.com>
2023-09-28 18:43:42 +0800

    remove useless code

--- sofa-serverless-runtime/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/command/handler/UninstallBizHandlerTests.java
+++ sofa-serverless-runtime/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/command/handler/UninstallBizHandlerTests.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.serverless.arklet.core.command.handler;
 
 import com.alipay.sofa.ark.api.ClientResponse;
@@ -27,7 +43,8 @@ public class UninstallBizHandlerTests extends BaseHandlerTest {
         input.setBizName("testBizName");
         input.setBizVersion("testBizVersion");
 
-        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion())).thenReturn(success);
+        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion()))
+            .thenReturn(success);
 
         Output<ClientResponse> result = handler.handle(input);
 
@@ -41,7 +58,8 @@ public class UninstallBizHandlerTests extends BaseHandlerTest {
         input.setBizName("testBizName");
         input.setBizVersion("testBizVersion");
 
-        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion())).thenReturn(failed);
+        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion()))
+            .thenReturn(failed);
 
         Output<ClientResponse> result = handler.handle(input);
 
