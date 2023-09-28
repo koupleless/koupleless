ce8651743d7b91965b2ebf2132b022a168af6e84
LiuLiqi <837397251@qq.com>
2023-09-28 14:56:56 +0800

    add isOnlyModuleUrlChange unit test and fix isModuleChange unit test

--- module-controller/internal/controller/moduledeployment_controller_test.go
+++ module-controller/internal/controller/moduledeployment_controller_test.go
@@ -9,7 +9,7 @@ import (
 	moduledeploymentv1alpha1 "github.com/sofastack/sofa-serverless/api/v1alpha1"
 )
 
-func TestIsModuleChanges(t *testing.T) {
+func TestIsModuleChange(t *testing.T) {
 	module1 := moduledeploymentv1alpha1.ModuleInfo{
 		Name:    "testModule1",
 		Version: "v1",
@@ -22,13 +22,28 @@ func TestIsModuleChanges(t *testing.T) {
 		Name:    "testModule2",
 		Version: "v2",
 	}
-	assert.False(t, isModuleChanges(module1, module1))
-	assert.True(t, isModuleChanges(module1, module2))
-	assert.True(t, isModuleChanges(module2, module3))
+	assert.False(t, isModuleChange(module1, module1))
+	assert.True(t, isModuleChange(module1, module2))
+	assert.True(t, isModuleChange(module2, module3))
 }
 
 func TestGetModuleReplicasName(t *testing.T) {
 	moduleDeploymentName := "module-deployment"
 	revision := 1
 	assert.Equal(t, fmt.Sprintf("%v-%v-%v", moduleDeploymentName, "replicas", revision), getModuleReplicasName(moduleDeploymentName, revision))
+}
+
+func TestIsOnlyModuleUrlChange(t *testing.T) {
+	module1 := moduledeploymentv1alpha1.ModuleInfo{
+		Name:    "testModule1",
+		Version: "v1",
+		Url:     "http://serverless-opensource.oss-cn-shanghai.aliyuncs.com/module-packages/stable/dynamic-provider-1.0.0-ark-biz.jar",
+	}
+	module2 := moduledeploymentv1alpha1.ModuleInfo{
+		Name:    "testModule1",
+		Version: "v1",
+		Url:     "http://serverless-opensource.oss-cn-shanghai.aliyuncs.com/module-packages/stable/dynamic-provider-1.0.1-ark-biz.jar",
+	}
+	assert.False(t, isOnlyModuleUrlChange(module1, module1))
+	assert.True(t, isModuleChange(module1, module2))
 }
