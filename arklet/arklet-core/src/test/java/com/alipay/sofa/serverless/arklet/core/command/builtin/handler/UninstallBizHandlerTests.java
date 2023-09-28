2a321f171d119bb24fe0562f4a92c4771be935f7
ColdClear <3124364150@qq.com>
2023-09-28 18:43:42 +0800

    remove useless code

--- arklet/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/command/builtin/handler/UninstallBizHandlerTests.java
+++ arklet/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/command/builtin/handler/UninstallBizHandlerTests.java
@@ -1,7 +1,24 @@
-package com.alipay.sofa.serverless.arklet.core.command.builtin.handler;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.serverless.arklet.core.command.handler;
 
 import com.alipay.sofa.ark.api.ClientResponse;
 import com.alipay.sofa.serverless.arklet.core.command.builtin.BuiltinCommand;
+import com.alipay.sofa.serverless.arklet.core.command.builtin.handler.UninstallBizHandler;
 import com.alipay.sofa.serverless.arklet.core.command.handler.BaseHandlerTest;
 import com.alipay.sofa.serverless.arklet.core.command.meta.Output;
 import com.alipay.sofa.serverless.arklet.core.common.exception.CommandValidationException;
@@ -26,7 +43,8 @@ public class UninstallBizHandlerTests extends BaseHandlerTest {
         input.setBizName("testBizName");
         input.setBizVersion("testBizVersion");
 
-        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion())).thenReturn(success);
+        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion()))
+            .thenReturn(success);
 
         Output<ClientResponse> result = handler.handle(input);
 
@@ -40,7 +58,8 @@ public class UninstallBizHandlerTests extends BaseHandlerTest {
         input.setBizName("testBizName");
         input.setBizVersion("testBizVersion");
 
-        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion())).thenReturn(failed);
+        when(handler.getOperationService().uninstall(input.getBizName(), input.getBizVersion()))
+            .thenReturn(failed);
 
         Output<ClientResponse> result = handler.handle(input);
 
