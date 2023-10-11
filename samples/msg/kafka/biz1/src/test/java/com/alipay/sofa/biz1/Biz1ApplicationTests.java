8a8dc552bebd049e5c9a836dccafb0650f442d1e
leojames <leojames.googol@gmail.com>
2023-10-11 14:59:01 +0800

    add samples for kafka

---
+++ samples/msg/kafka/biz1/src/test/java/com/alipay/sofa/biz1/Biz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.biz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
