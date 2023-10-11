ca2cea0e01da067190b7815f904c9ee73fae6aff
youji.zzl <youji.zzl@antfin.com>
2023-10-11 14:59:01 +0800

    add samples for kafka

---
+++ samples/msg/kafka/biz2/src/test/java/com/alipay/sofa/biz2/Biz2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.biz2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biz2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
