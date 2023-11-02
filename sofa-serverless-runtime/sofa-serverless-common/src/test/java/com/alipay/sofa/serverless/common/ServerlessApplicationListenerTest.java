6decdd19b25416ae3226d35c7f8ffff342a4d0b9
yuanyuan <fengjun.zfj@antgroup.com>
2023-11-02 21:45:44 +0800

    add ut for ServerlessApplicationListener

--- sofa-serverless-runtime/sofa-serverless-common/src/test/java/com/alipay/sofa/serverless/common/ServerlessApplicationListenerTest.java
+++ sofa-serverless-runtime/sofa-serverless-common/src/test/java/com/alipay/sofa/serverless/common/ServerlessApplicationListenerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.serverless.common;
 
 import com.alipay.sofa.ark.api.ArkConfigs;
@@ -20,7 +36,8 @@ public class ServerlessApplicationListenerTest {
     @Test
     public void testOnApplicationEvent() {
         ServerlessApplicationListener listener = new ServerlessApplicationListener();
-        ApplicationStartingEvent applicationStartingEvent = new ApplicationStartingEvent(mock(ConfigurableBootstrapContext.class), mock(SpringApplication.class), new String[0]);
+        ApplicationStartingEvent applicationStartingEvent = new ApplicationStartingEvent(
+            mock(ConfigurableBootstrapContext.class), mock(SpringApplication.class), new String[0]);
         listener.onApplicationEvent(applicationStartingEvent);
         Assert.assertTrue(ArkConfigs.isEmbedEnable());
         Assert.assertEquals("true", System.getProperty(PLUGIN_EXPORT_CLASS_ENABLE));
