39abbbe804a1f72a4af9ca8ae5c3f6fd7cba8015
尚之 <bingjie.lbj@antgroup.com>
2023-11-05 16:52:10 +0800

    新增dubbo3的samples

---
+++ samples/dubbo-samples/rpc/dubbo3/common-model/src/main/java/com/alipay/sofa/rpc/dubbo3/model/CommonService.java
@@ -0,0 +1,15 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package com.alipay.sofa.rpc.dubbo3.model;
+
+/**
+ *
+ * @author syd
+ * @version CommonService.java, v 0.1 2023年11月05日 15:17 syd
+ */
+public interface CommonService {
+
+    CommonResponse sayHello(CommonRequest request);
+}
