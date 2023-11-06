dc0db89150555f1bbfdf2451d899a1f8a478ec90
mingmen <mingmen.zjm@alibaba-inc.com>
2023-11-06 14:45:26 +0800

    补充代码

--- sofa-serverless-runtime/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/netty/NettyServerTest.java
+++ sofa-serverless-runtime/arklet-core/src/test/java/com/alipay/sofa/serverless/arklet/core/netty/NettyServerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.serverless.arklet.core.netty;
 
 import com.alipay.sofa.serverless.arklet.core.BaseTest;
@@ -26,8 +42,9 @@ public class NettyServerTest extends BaseTest {
     public void command() throws Exception {
         NettyHttpHandler handler = new NettyHttpHandler(commandService);
         String content = "{\"bizName\":\"test\",\"bizVersion\":\"1.0.0\",\"bizUrl\":\"http://serverless-opensource"
-            + ".oss-cn-shanghai.aliyuncs.com/module-packages/stable/dynamic-provider-1.0.0-ark-biz.jar\"}";
-        DefaultFullHttpRequest fullHttpRequest = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/installBiz", Unpooled.copiedBuffer(content.getBytes()));
+                         + ".oss-cn-shanghai.aliyuncs.com/module-packages/stable/dynamic-provider-1.0.0-ark-biz.jar\"}";
+        DefaultFullHttpRequest fullHttpRequest = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1,
+            HttpMethod.POST, "/installBiz", Unpooled.copiedBuffer(content.getBytes()));
         handler.channelRead(mockCtx, fullHttpRequest);
     }
 
