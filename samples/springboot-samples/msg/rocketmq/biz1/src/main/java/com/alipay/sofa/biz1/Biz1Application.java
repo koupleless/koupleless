cc09d17a97cca3e937bc869f1216f1425311d7e0
leojames <leojames.googol@gmail.com>
2023-11-09 15:11:49 +0800

    remove embeded and export arguments

--- samples/springboot-samples/msg/rocketmq/biz1/src/main/java/com/alipay/sofa/biz1/Biz1Application.java
+++ samples/springboot-samples/msg/rocketmq/biz1/src/main/java/com/alipay/sofa/biz1/Biz1Application.java
@@ -11,10 +11,6 @@ public class Biz1Application {
 	private static Logger LOGGER = LoggerFactory.getLogger(Biz1Application.class);
 
 	public static void main(String[] args) {
-		// 设置内嵌方式启动多模块运行容器, 也可以放在启动参数中
-		System.setProperty("sofa.ark.embed.enable", "true");
-		System.setProperty("sofa.ark.plugin.export.class.enable", "true");
-
 		SpringApplication.run(Biz1Application.class, args);
 		LOGGER.info("Biz1Application start!");
 		LOGGER.info("Spring Boot Version: " + SpringApplication.class.getPackage().getImplementationVersion());
