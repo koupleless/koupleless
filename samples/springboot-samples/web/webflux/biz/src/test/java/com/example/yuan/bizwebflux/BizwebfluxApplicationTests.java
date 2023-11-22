c744860559f81ac9ca0e639c93c2ee93df873f6e
yuanyuan <fengjun.zfj@antgroup.com>
2023-11-22 17:23:46 +0800

    samples for webflux in single host

---
+++ samples/springboot-samples/web/webflux/biz/src/test/java/com/example/yuan/bizwebflux/BizwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuan.bizwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
