6741dd0bdf6a754ccfeb69b8096a3c1b71f5b0f6
leojames <leojames.googol@gmail.com>
2024-02-03 01:53:57 +0800

    only adapter for jdk17

---
+++ koupleless-runtime/koupleless-adapter-ext/koupleless-adapter-logback/src/main/java/ch/qos/logback/classic/spi/LogbackServiceProvider.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.qos.logback.classic.spi;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.status.StatusUtil;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.BasicMarkerFactory;
+import org.slf4j.helpers.Util;
+import org.slf4j.spi.MDCAdapter;
+import org.slf4j.spi.SLF4JServiceProvider;
+
+public class LogbackServiceProvider implements SLF4JServiceProvider {
+
+    final static String                       NULL_CS_URL           = CoreConstants.CODES_URL
+                                                                      + "#null_CS";
+
+    /**
+     * Declare the version of the SLF4J API this implementation is compiled against.
+     * The value of this field is modified with each major release.
+     */
+    // to avoid constant folding by the compiler, this field must *not* be final
+    public static String                      REQUESTED_API_VERSION = "2.0.99";         // !final
+
+    private LoggerContext                     defaultLoggerContext;
+    private IMarkerFactory                    markerFactory;
+    private LogbackMDCAdapter                 mdcAdapter;
+    private final ContextSelectorStaticBinder contextSelectorBinder = ContextSelectorStaticBinder
+                                                                        .getSingleton();
+    private static Object                     KEY                   = new Object();
+    private volatile boolean                  initialized           = false;
+
+    @Override
+    public void initialize() {
+        defaultLoggerContext = new LoggerContext();
+        defaultLoggerContext.setName(CoreConstants.DEFAULT_CONTEXT_NAME);
+        initializeLoggerContext();
+        defaultLoggerContext.start();
+        markerFactory = new BasicMarkerFactory();
+        mdcAdapter = new LogbackMDCAdapter();
+        // set the MDCAdapter for the defaultLoggerContext immediately
+        defaultLoggerContext.setMDCAdapter(mdcAdapter);
+        System.out.println("init logback in sofa serverless adapter");
+    }
+
+    private void initializeLoggerContext() {
+        try {
+            try {
+                new ContextInitializer(defaultLoggerContext).autoConfig();
+            } catch (JoranException je) {
+                Util.report("Failed to auto configure default logger context", je);
+            }
+            // LOGBACK-292
+            if (!StatusUtil.contextHasStatusListener(defaultLoggerContext)) {
+                StatusPrinter.printInCaseOfErrorsOrWarnings(defaultLoggerContext);
+            }
+            contextSelectorBinder.init(defaultLoggerContext, KEY);
+            initialized = true;
+        } catch (Exception t) { // see LOGBACK-1159
+            Util.report("Failed to instantiate [" + LoggerContext.class.getName() + "]", t);
+        }
+    }
+
+    @Override
+    public ILoggerFactory getLoggerFactory() {
+        if (!initialized) {
+            return defaultLoggerContext;
+        }
+
+        if (contextSelectorBinder.getContextSelector() == null) {
+            throw new IllegalStateException("contextSelector cannot be null. See also "
+                                            + NULL_CS_URL);
+        }
+        return contextSelectorBinder.getContextSelector().getLoggerContext();
+    }
+
+    @Override
+    public IMarkerFactory getMarkerFactory() {
+        return markerFactory;
+    }
+
+    @Override
+    public MDCAdapter getMDCAdapter() {
+        return mdcAdapter;
+    }
+
+    @Override
+    public String getRequestedApiVersion() {
+        return REQUESTED_API_VERSION;
+    }
+
+}
