117c16a0a4c519db651c1173d06795bb01a831fc
suntaiming <suntaiming0306@163.com>
2024-02-18 10:29:21 +0800

    适配log4j2增强(根据当前线程上下文类加载器动态获取Logger)及相关文档补充

--- samples/springboot-samples/logging/log4j2/biz2/src/main/java/com/alipay/sofa/logging/biz2/rest/SampleController.java
+++ samples/springboot-samples/logging/log4j2/biz2/src/main/java/com/alipay/sofa/logging/biz2/rest/SampleController.java
@@ -16,6 +16,7 @@
  */
 package com.alipay.sofa.logging.biz2.rest;
 
+import net.sf.ehcache.CacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,10 @@ public class SampleController {
     public String hello() {
         String appName = applicationContext.getApplicationName();
         LOGGER.info("{} web test: into sample controller", appName);
+
+        CacheManager.create();
+        CacheManager.create();
+
         return String.format("hello to %s deploy", appName);
     }
 }
