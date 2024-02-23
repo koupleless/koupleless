ed6404cc171da50cbbec662bd7592ac71bd2484b
巨鹿 <yanwen@cai-inc.com>
2024-02-23 17:43:50 +0800

    将forward的配置放在spring的配置中

---
+++ koupleless-runtime/koupleless-ext/koupleless-ext-web-gateway/src/main/java/com/alipay/sofa/koupleless/ext/web/gateway/ForwardAutoConfiguration.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.ext.web.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@EnableConfigurationProperties(GatewayProperties.class)
+@ConditionalOnProperty(name = "koupleless.forward.conf.path")
+@ComponentScan(basePackages = "com.alipay.sofa.koupleless.ext.web")
+public class ForwardAutoConfiguration {
+    private static final String EMPTY               = "";
+    private static final String CONTEXT_PATH_PREFIX = "/";
+
+    private static final String PATH_PREFIX         = "/";
+    @Autowired
+    private GatewayProperties   gatewayProperties;
+
+    @Bean
+    public Forwards forwards() {
+        //load conf
+        List<Forward> forwards = gatewayProperties.getForwards();
+        List<ForwardItem> items = toForwardItems(forwards);
+        items.sort(ForwardItemComparator.getInstance());
+        //make forwards bean and set conf
+        Forwards bean = new Forwards();
+        bean.setItems(items);
+        return bean;
+    }
+
+    private List<ForwardItem> toForwardItems(List<Forward> forwards) {
+        if (CollectionUtils.isEmpty(forwards)) {
+            return Collections.emptyList();
+        }
+        List<ForwardItem> items = new LinkedList<>();
+        for (Forward forward : forwards) {
+            items.addAll(toForwardItems(forward, items.size()));
+        }
+        return items;
+    }
+
+    private List<ForwardItem> toForwardItems(Forward forward, int startIndex) {
+        Set<String> hosts = forward.getHosts();
+        if (CollectionUtils.isEmpty(hosts)) {
+            hosts = Collections.singleton(EMPTY);
+        }
+        String contextPath = forward.getContextPath();
+        if (!contextPath.startsWith(CONTEXT_PATH_PREFIX)) {
+            contextPath = CONTEXT_PATH_PREFIX + contextPath;
+        }
+        Set<ForwardPath> paths = forward.getPaths();
+        if (CollectionUtils.isEmpty(paths)) {
+            ForwardPath path = new ForwardPath();
+            path.setFrom(PATH_PREFIX);
+            path.setTo(PATH_PREFIX);
+            paths = Collections.singleton(path);
+        } else {
+            for (ForwardPath path : paths) {
+                String from = path.getFrom();
+                if (!from.startsWith(CONTEXT_PATH_PREFIX)) {
+                    path.setFrom(CONTEXT_PATH_PREFIX + from);
+                }
+                String to = path.getTo();
+                if (to == null) {
+                    path.setTo(path.getFrom());
+                    continue;
+                }
+                if (!to.startsWith(CONTEXT_PATH_PREFIX)) {
+                    path.setTo(CONTEXT_PATH_PREFIX + to);
+                }
+            }
+        }
+        List<ForwardItem> items = new LinkedList<>();
+        for (String host : hosts) {
+            for (ForwardPath path : paths) {
+                ForwardItem item = new ForwardItem(contextPath, host, path.getFrom(), path.getTo());
+                items.add(item);
+            }
+        }
+        return items;
+    }
+}
---
+++ koupleless-runtime/koupleless-ext/koupleless-ext-web-gateway/src/test/java/com/alipay/sofa/koupleless/ext/web/ForwardControllerTests.java
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.ext.web;
+
+import com.alipay.sofa.koupleless.ext.web.gateway.ForwardAutoConfiguration;
+import com.alipay.sofa.koupleless.ext.web.gateway.ForwardController;
+import com.alipay.sofa.koupleless.ext.web.gateway.Forwards;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ForwardControllerTests {
+    @InjectMocks
+    private ForwardAutoConfiguration configuration;
+    @Mock
+    private ApplicationContext       applicationContext;
+    private String                   confPath   = "classpath:forwards.yaml";
+
+    private ForwardController        controller = new ForwardController();
+
+    @Before
+    public void before() throws NoSuchFieldException, IllegalAccessException, IOException {
+        Field field = ForwardAutoConfiguration.class.getDeclaredField("confPath");
+        field.setAccessible(true);
+        field.set(configuration, confPath);
+        Mockito.when(applicationContext.getResources(Mockito.anyString())).thenReturn(
+            new Resource[] { new ClassPathResource("forwards.yaml") });
+        Forwards forwards = configuration.forwards();
+        field = ForwardController.class.getDeclaredField("forwards");
+        field.setAccessible(true);
+        field.set(controller, forwards);
+    }
+
+    @Test
+    public void testRedirect() throws IOException, ServletException {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        ServletContext baseContext = Mockito.mock(ServletContext.class);
+        Mockito.when(request.getServletContext()).thenReturn(baseContext);
+
+        ServletContext context1 = Mockito.mock(ServletContext.class);
+        RequestDispatcher dispatcher1 = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(context1.getRequestDispatcher((Mockito.anyString()))).thenReturn(dispatcher1);
+
+        ServletContext context2 = Mockito.mock(ServletContext.class);
+        RequestDispatcher dispatcher2 = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(context2.getRequestDispatcher((Mockito.anyString()))).thenReturn(dispatcher2);
+
+        ServletContext context3 = Mockito.mock(ServletContext.class);
+        RequestDispatcher dispatcher3 = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(context3.getRequestDispatcher((Mockito.anyString()))).thenReturn(dispatcher3);
+
+        Mockito.when(baseContext.getContext(Mockito.anyString())).then((invocation) -> {
+            String uri = invocation.getArgument(0);
+            if (uri.startsWith("/test1/")) {
+                return context1;
+            }
+            if (uri.startsWith("/test2/")) {
+                return context2;
+            }
+            if (uri.startsWith("/test3/")) {
+                return context3;
+            }
+            return baseContext;
+        });
+
+
+        Mockito.when(request.getRequestURL()).thenReturn(new StringBuffer("http://test1.xxx.com/test2"));
+        controller.redirect(request, response);
+        Mockito.verify(dispatcher2, Mockito.times(1)).forward(request, response);
+
+
+        Mockito.when(request.getRequestURL()).thenReturn(new StringBuffer("http://test1.xxx.com/test1/xx"));
+        controller.redirect(request, response);
+        Mockito.verify(dispatcher1, Mockito.times(1)).forward(request, response);
+
+
+        Mockito.when(request.getRequestURL()).thenReturn(new StringBuffer("http://test3.xxx.com/test1/xx"));
+        controller.redirect(request, response);
+        Mockito.verify(dispatcher3, Mockito.times(1)).forward(request, response);
+
+
+        Mockito.when(request.getRequestURL()).thenReturn(new StringBuffer("http://test4.xxx.com/test1/xx"));
+        Assert.assertThrows(ResponseStatusException.class, () -> controller.redirect(request, response));
+    }
+}
